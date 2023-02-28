50ac3565d67e7b700b1c739b616c12ae706f84f2
huseyincicek01 <65832150+huseyincicek01@users.noreply.github.com>
2023-02-28 14:19:41 +0300

    Update TheKillStreak.lua

--- TheKillStreak.lua
+++ TheKillStreak.lua
@@ -74,4 +74,4 @@ game:GetService("Players"):WaitForChild("huseyincicek01"):WaitForChild("PlayerGu
     end
 end)
 MainSection:NewButton("Shutdown", "Shutdown", function()
-        game:GetService("Players").huseyincicek01.PlayerGui.AdminPanel.AdminPanelGui.Events.ShutdownEvent.FireServer:("Test")
+        game:GetService("Players").huseyincicek01.PlayerGui.AdminPanel.AdminPanelGui.Events.ShutdownEvent
