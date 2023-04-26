13c2f0d725b936425afab0159642bbac3ab39ce9
huseyincicek01 <65832150+huseyincicek01@users.noreply.github.com>
2023-04-26 16:57:13 +0300

    Update LuckyBlock.lua

--- LuckyBlock.lua
+++ LuckyBlock.lua
@@ -1,8 +1,8 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
 local Window = Library.CreateLib("Gay Hub | This Gui Made By Cat#2728 And Mac#0014", "DarkTheme")
+
 Main = Window:NewTab("Lucky Block")
 local MainSection = Main:NewSection("Free Lucky Blocks")
 MainSection:NewButton("Galaxy Block", "Free Galaxy Block", function()
 game:GetService("ReplicatedStorage").SpawnGalaxyBlock:FireServer()
 end)
-end
